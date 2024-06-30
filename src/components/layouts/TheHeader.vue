4158e4ecab10daf34a24bd596b3e82da894112a1
Aleksander Afanasyev <afonia.marcii@gmail.com>
2024-06-30 12:02:17 +0200

    feat: logo reload

--- src/components/layouts/TheHeader.vue
+++ src/components/layouts/TheHeader.vue
@@ -1,6 +1,16 @@
 <template>
   <header class="header">
-    <h1 class="header__logo">Жилфонд</h1>
+    <h1 class="header__logo" @click="reloadData">Жилфонд</h1>
     <p class="header__user-role">Пользователь</p>
   </header>
 </template>
+
+<script>
+export default {
+  methods: {
+    reloadData() {
+      window.location.reload();
+    },
+  },
+};
+</script>
